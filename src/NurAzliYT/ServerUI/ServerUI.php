29f163801d905c4c4d067479e79dc09d4ad95778
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-23 05:11:40 +0700

    Update ServerUI.php

--- src/NurAzliYT/ServerUI/ServerUI.php
+++ src/NurAzliYT/ServerUI/ServerUI.php
@@ -30,31 +30,31 @@ class ServerUI extends PluginBase{
       switch ($data){
           case 0;
           $inv = $player->getInventory();
-          $inv->setItem(Index:0, VanillaItems::DIAMOND()->setCount:(30));
+          $inv->setItem(Index:0, VanillaItems::DIAMOND()->setCount(30));
           $player->sendMessange("Successfully Gives 30 Diamonds");
           break;
           
           case 1;
           $inv = $player->getInventory();
-          $inv->setItem(Index:0, VanillaItems::GOLD_INGOT()->setCount:(30));
+          $inv->setItem(Index:0, VanillaItems::GOLD_INGOT()->setCount(30));
           $player->sendMessange("Successfully Gives 30 Gold");
           break;
           
           case 2;
           $inv = $player->getInventory();
-          $inv->setItem(Index:0, VanillaItems::IRON_INGOT()->setCount:(30));
+          $inv->setItem(Index:0, VanillaItems::IRON_INGOT()->setCount(30));
           $player->sendMessange("Successfully Gives 30 Iron Ingot");
           break;
           
           case 3;
           $inv = $player->getInventory();
-          $inv->setItem(Index:0, VanillaItems::EMERALD()->setCount:(30));
+          $inv->setItem(Index:0, VanillaItems::EMERALD()->setCount(30));
           $player->sendMessange("Successfully Gives 30 Emerald");
           break;
           
           case 4;
           $inv = $player->getInventory();
-          $inv->setItem(Index:0, VanillaItems::NETHERITE_INGOT()->setCount:(30));
+          $inv->setItem(Index:0, VanillaItems::NETHERITE_INGOT()->setCount(30));
           $player->sendMessange("Successfully Gives 30 netherite Ingot");
           break;
       }
