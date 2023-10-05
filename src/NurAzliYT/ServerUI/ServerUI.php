8a5640c25e1cb3bfdc9e26b2fef4d2fafde7efa8
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-06 04:49:17 +0700

    Update ServerUI.php

--- src/NurAzliYT/ServerUI/ServerUI.php
+++ src/NurAzliYT/ServerUI/ServerUI.php
@@ -1,74 +1,165 @@
 <?php
+declare(strict_types=1);
 
-namespace NurAzliYT\ServerUI;
+namespace NurAzliYT\InfoUI;
 
+use pocketmine\Server;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use jojoe77777\FormAPI\SimpleForm;
-use pocketmine\item\VanillaItems;
+use pocketmine\event\Listener;
 
-class ServerUI extends PluginBase{
-  public function OnEnable():void{
-  this->saveDefaultConfig()
-    this->getResource(config.yml)
-  }
-  public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool{
+use jojoe77777\FormAPI\SimpleForm;
+
+class Loader extends PluginBase implements Listener{
     
-    if($command-getName() == "ui"){
-      if($sender instanceof Player){
-        $this->onSimpleForm($sender);
-      }
+    public function onEnable() : void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        
+        @mkdir($this->getDataFolder());
+       $this->saveDefaultConfig();
+       $this->getResource("config.yml");
+       
     }
-    return true;
-  }
-  
-  public function onSimpleForm(Player $player):void{
-    $form = new SimpleForm(function (Player $player, $data){
-      if(!isset($data)){
-        return;
-      }
-      switch ($data){
-          case 0:
-          $inv = $player->getInventory();
-          $inv->setItem(index:0, VanillaItems::DIAMOND()->setCount(30));
-          $player->sendMessange("Successfully Gives 30 Diamonds");
-          break;
-          
-          case 1:
-          $inv = $player->getInventory();
-          $inv->setItem(index:0, VanillaItems::GOLD_INGOT()->setCount(30));
-          $player->sendMessange("Successfully Gives 30 Gold");
-          break;
-          
-          case 2:
-          $inv = $player->getInventory();
-          $inv->setItem(index:0, VanillaItems::IRON_INGOT()->setCount(30));
-          $player->sendMessange("Successfully Gives 30 Iron Ingot");
-          break;
-          
-          case 3:
-          $inv = $player->getInventory();
-          $inv->setItem(index:0, VanillaItems::EMERALD()->setCount(30));
-          $player->sendMessange("Successfully Gives 30 Emerald");
-          break;
-          
-          case 4:
-          $inv = $player->getInventory();
-          $inv->setItem(index:0, VanillaItems::NETHERITE_INGOT()->setCount(30));
-          $player->sendMessange("Successfully Gives 30 netherite Ingot");
-          break;
-      }
     
-    });                      
-                           
-  $form->setTitle("ServerUI");
-  $form->setContent("Server Menu");
-  $form->addButton("Claim Free Diamonds");
-  $form->addButton("Claim Free Gold");
-  $form->addButton("Claim Free Iron Ingot");
-  $form->addButton("Claim Free Emerald");
-  $form->addButton("Claim Free Netherite");
-  $player->sendForm($form);
-  }
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+        
+        if($cmd->getName() == "info"){
+            $this->InfoUI($sender);
+        }
+        
+        return true;
+    }
+    
+    public function InfoUI($player){
+        $form = new SimpleForm(function(Player $player, int $data = null){
+        $result = $data;
+        if($result === null){
+            return true;
+            }
+            switch($result){
+                case 0:
+                    $this->About($player);
+                break;
+                
+                case 1:
+                    $this->Rules($player);
+                break;
+                
+                case 2:
+                    $this->Staff($player);
+                break;
+                
+                case 3:
+                    $this->Update($player);
+                break;
+                
+                case 4:
+                    $this->Announcement($player);
+                break;
+            }
+        });
+        $form->setTitle($this->getConfig()->get("InfoUI-Title"));
+        $form->addButton($this->getConfig()->get("About-Button"), 0, "textures/ui/icon_book_writable");
+        $form->addButton($this->getConfig()->get("Rules-Button"), 0, "textures/ui/recipe_book_icon");
+        $form->addButton($this->getConfig()->get("Staff-Button"), 0, "textures/ui/icon_best3");
+        $form->addButton($this->getConfig()->get("Update-Button"), 0, "textures/ui/mashup_world");
+        $form->addButton($this->getConfig()->get("Announcement-Button"), 0, "textures/ui/icon_book_writable");
+        $form->addButton($this->getConfig()->get("Exit-Button"), 0, "textures/ui/icon_import");
+        $form->sendToPlayer($player);
+    }
+    
+    public function About($player){
+        $form = new SimpleForm(function(Player $player, int $data = null){
+        $result = $data;
+        if($data === null){
+            return true;
+            }
+            switch($result){
+                case 0:
+                    $this->InfoUI($player);
+                break;
+            }
+        });
+        $form->setTitle($this->getConfig()->get("About-Title"));
+        $form->setContent($this->getConfig()->get("About-Text"));
+        $form->addButton($this->getConfig()->get("Back-Button"), 0, "textures/ui/refresh_light");
+        $form->sendToPlayer($player);
+    }
+    
+    public function Rules($player){
+        $form = new SimpleForm(function(Player $player, int $data = null){
+        $result = $data;
+        if($result === null){
+            return true;
+            }
+            switch($result){
+                case 0:
+                    $this->InfoUI($player);
+                break;
+            }
+        });
+        $form->setTitle($this->getConfig()->get("Rules-Title"));
+        $form->setContent($this->getConfig()->get("Rules-Text"));
+        $form->addButton($this->getConfig()->get("Back-Button"), 0, "textures/ui/refresh_light");
+        $form->sendToPlayer($player);
+    }
+    
+    public function Staff($player){
+        $form = new SimpleForm(function(Player $player, int $data = null){
+        $result = $data;
+        if($result === null){
+            return true;
+            }
+            switch($result){
+                case 0:
+                    $this->InfoUI($player);
+                break;
+            }
+        });
+        $form->setTitle($this->getConfig()->get("Staff-Title"));
+        $form->setContent($this->getConfig()->get("Staff-Text"));
+        $form->addButton($this->getConfig()->get("Back-Button"), 0, "textures/ui/refresh_light");
+        $form->sendToPlayer($player);
+    }
+    
+    public function Update($player){
+        $form = new SimpleForm(function(Player $player, int $data = null){
+        $result = $data;
+        if($result === null){
+            return true;
+            }
+            switch($result){
+                case 0:
+                    $this->InfoUI($player);
+                break;
+            }
+        });
+        # Form Set For Info
+        $form->setTitle($this->getConfig()->get("Update-Title"));
+        $form->setContent($this->getConfig()->get("Update-Text"));
+        $form->addButton($this->getConfig()->get("Back-Button"), 0, "textures/ui/refresh_light");
+        $form->sendToPlayer($player);
+    }
+        
+    public function Announcement($player){
+        $form = new SimpleForm(function(Player $player, int $data = null){
+        $result = $data;
+        if($result === null){
+            return true;
+            }
+            switch($result){
+                case 0:
+                    $this->InfoUI($player);
+                break;
+            }
+        });
+        # Form Set For Info
+        $form->setTitle($this->getConfig()->get("Announcement-Title"));
+        $form->setContent($this->getConfig()->get("Announcement-Text"));
+        $form->addButton($this->getConfig()->get("Back-Button"), 0, "textures/ui/refresh_light");
+        $form->sendToPlayer($player);
+    }
 }
